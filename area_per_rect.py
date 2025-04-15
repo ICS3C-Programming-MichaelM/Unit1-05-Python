a1e653e295ef1c706c08fe179d57d626978ab2ac
Micthelle michael <micthelle.chinwendu.michael@stu.ocsb.ca>
2025-04-15 17:06:05 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Micthelle Michael
+# Date: april 15, 2025
+
+
+def main():
+    print("This program calculates the area and perimeter of a rectangle.")
+    print("with length 5cm and width 3cm")
+    length = 5
+    width = 3
+    area = length * width
+    perimeter = 2 * (length + width)
+    print("Area = {}cm^2".format(area))
+    print("Perimeter = {}cm".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
